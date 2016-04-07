09be7e56ee513d84680dab8a35b2fe14fb9b7b21
Bruno Marques <bruno@bmarques.net>
2016-04-06 23:11:37 -0300

    Added SimpleSearchResource; renamed SearchViewResource to SearchResource

--- restless_haystack/resources.py
+++ restless_haystack/resources.py
@@ -4,12 +4,15 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import warnings
 
+from django.core.paginator import Paginator, InvalidPage
 from haystack.forms import SearchForm
-from haystack.views import SearchView
+from haystack.query import SearchQuerySet
+from haystack.views import SearchView, RESULTS_PER_PAGE
 from restless.dj import DjangoResource
+from restless.exceptions import NotFound
 
 
-class SearchViewResource(SearchView, DjangoResource):
+class SearchResource(SearchView, DjangoResource):
     """
     A RESTful resource which behaves like Haystack's ``SearchView``, including
      the use of ``SearchForm`` (and subclasses) for validation.
@@ -53,9 +56,6 @@ class SearchViewResource(SearchView, DjangoResource):
         return self.serializer.serialize(final_data)
 
     def create_response(self):
-        """
-        Generates the actual HttpResponse to send back to the user.
-        """
         (paginator, page) = self.build_page()
 
         context = {
@@ -75,15 +75,81 @@ class SearchViewResource(SearchView, DjangoResource):
         return context
 
 
-def searchview_resource_factory(searchqueryset=None, form_class=SearchForm,
-                                load_all=True):
+class SimpleSearchResource(DjangoResource):
+    searchqueryset = SearchQuerySet()
+    filters = []
+    results_per_page = RESULTS_PER_PAGE
+    load_all = False
+
+    def list(self):
+        filters = {attr: self.request.GET[attr] for attr in self.filters
+                   if attr in self.request.GET}
+        self.searchqueryset = self.searchqueryset.filter(**filters)
+
+        if self.request.GET.get('q'):
+            self.searchqueryset = self.searchqueryset.filter(
+                content=self.request.GET.get('q'))
+
+        if self.load_all:
+            self.searchqueryset = self.searchqueryset.load_all()
+        return self.get_page()
+
+    def get_page(self):
+        try:
+            page_no = int(self.request.GET.get('page', 1))
+        except (TypeError, ValueError):
+            raise NotFound('Invalid page number.')
+        if page_no < 1:
+            raise NotFound('Page number should be 1 or greater.')
+        paginator = Paginator(self.searchqueryset, self.results_per_page)
+
+        try:
+            page = paginator.page(page_no)
+        except InvalidPage:
+            raise NotFound('No such page!')
+        return page
+
+    def serialize_list(self, data):
+        """
+        Serializes the search results returned by ``get_page``. The output
+         contains the following fields:
+
+        * ``objects``: a list of prepared results (same as ``Resource``)
+        * ``page``: number of the returned page
+        * ``start_index``: initial (1-based) index for the returned page
+        * ``end_index``: final (1-based) index for the returned page
+        * ``num_pages``: total number of pages
+        * ``query``: the provided query
+
+        :param data: The resulting search page
+        :type data: Page
+
+        :returns: The serialized body
+        :rtype: string
+        """
+        if data is None:
+            return ''
+
+        final_data = {
+            'objects': [self.prepare(obj) for obj in data.object_list],
+            'page': data.number,
+            'start_index': data.start_index(),
+            'end_index': data.end_index(),
+            'num_pages': data.paginator.num_pages,
+            'query': self.request.GET.get('q'),
+        }
+        return self.serializer.serialize(final_data)
+
+
+def search_resource_factory(searchqueryset=None, form_class=SearchForm,
+                            load_all=True):
     """
-    A factory for creating ``SearchViewResource`` subclasses with the given
+    A factory for creating ``SearchResource`` subclasses with the given
     properties.
 
-    :return: A ``SearchViewResource`` subclass
+    :return: A ``SearchResource`` subclass
     """
-    class Resource(SearchViewResource):
+    class Resource(SearchResource):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             self.searchqueryset = searchqueryset
@@ -97,10 +163,10 @@ DEPRECATION_MESSAGE = '`{}` is deprecated; `{}` replaces it.' + \
                       'The old name is set for removal on version 0.3'
 
 
-class HaystackResource(SearchViewResource):
+class HaystackResource(SearchResource):
     def __init__(self, *args, **kwargs):
         warnings.warn(DEPRECATION_MESSAGE.format(
-            'HaystackResource', 'SearchViewResource'),
+            'HaystackResource', 'SearchResource'),
             DeprecationWarning)
         super().__init__(*args, **kwargs)
 
@@ -108,6 +174,6 @@ class HaystackResource(SearchViewResource):
 def haystack_resource_factory(searchqueryset=None, form_class=SearchForm,
                               load_all=True):
     warnings.warn(DEPRECATION_MESSAGE.format(
-        'haystack_resource_factory', 'searchview_resource_factory'),
+        'haystack_resource_factory', 'search_resource_factory'),
         DeprecationWarning)
-    return searchview_resource_factory(searchqueryset, form_class, load_all)
+    return search_resource_factory(searchqueryset, form_class, load_all)
